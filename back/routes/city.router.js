ba3e745e2c61d84279ba1a71463d5932c530ca39
Thanasis Polychronakis <thanpolas@gmail.com>
2014-11-30 10:57:36 +0200

    Add community CRUD routes

--- back/routes/city.router.js
+++ back/routes/city.router.js
@@ -6,6 +6,7 @@ var log = require('logg').getLogger('app.router.city');
 var HomeCtrl = require('../controllers/city/index.ctrl');
 var StaticsCtrl = require('../controllers/city/statics.ctrl');
 var DashboardCtrl = require('../controllers/city/dashboard.ctrl');
+var CommunityCtrl = require('../controllers/city/community.ctrl');
 
 var router = module.exports = {};
 
@@ -19,10 +20,16 @@ router.init = function(app) {
   var homeCtrl = HomeCtrl.getInstance();
   var staticsCtrl = StaticsCtrl.getInstance();
   var dashboardCtrl = DashboardCtrl.getInstance();
+  var communityCtrl = CommunityCtrl.getInstance();
 
   app.get('/', homeCtrl.use);
 
   app.get('/submit-event', staticsCtrl.use);
 
   app.get('/dashboard', dashboardCtrl.use);
+
+  app.get('/community/:id/edit', communityCtrl.edit);
+  app.get('/community/:id/delete', communityCtrl.del);
+  app.get('/community/add', communityCtrl.add);
+  communityCtrl.crude.addRoutes(app);
 };
