06b528727a2ce19e2cebbb8e3f23dee91d0dde5f
Thanasis Polychronakis <thanpolas@gmail.com>
2014-11-30 11:08:54 +0200

    Implement the fetch single community middleware

--- back/middleware/communities.midd.js
+++ back/middleware/communities.midd.js
@@ -25,7 +25,6 @@ var Community = module.exports = MiddlewareBase.extendSingleton(function () {
  * @param {Object} req The request Object.
  * @param {Object} res The response Object.
  * @param {Function} next Pass control.
- * @private
  */
 Community.prototype.populate = function (req, res, next) {
   CommunityEnt.getInstance().read({cityOwner: req.city._id})
@@ -39,7 +38,36 @@ Community.prototype.populate = function (req, res, next) {
       next();
     })
     .catch(function (err) {
-      log.warn('_fetchCommunities() :: Error on fetching communities:', err);
+      log.warn('populate() :: Error on fetching communities:', err);
+      if (typeof err.toApi === 'function') {
+        err = err.toApi();
+      }
+      res.status(500).render('city/error/500', {error: err});
+    });
+};
+
+/**
+ * Will populate a single community item based on the params.id var of the url.
+ *
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ * @param {Function} next Pass control.
+ * @private
+ */
+Community.prototype.populateSingleFromParam = function (req, res, next) {
+  CommunityEnt.getInstance().readOne({_id: req.params.id})
+    .bind(this)
+    .then(function(result) {
+      if (!result) {
+        res.status(404).render('/city/error/404');
+      } else {
+        res.locals.community = result;
+        next();
+      }
+    })
+    .catch(function (err) {
+      log.warn('populateSingleFromParam() :: Error on fetching community, id:',
+        req.params.id, 'Err:', err);
       if (typeof err.toApi === 'function') {
         err = err.toApi();
       }
