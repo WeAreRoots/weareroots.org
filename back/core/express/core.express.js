6f0c03ebb5ff70adaec916c981c30a5f2ea7454d
Thanasis Polychronakis <thanpolas@gmail.com>
2014-11-30 16:21:08 +0200

    more desperate tries

--- back/core/express/core.express.js
+++ back/core/express/core.express.js
@@ -98,6 +98,7 @@ ExpressApp.prototype.init = BPromise.method(function(opts) {
       // SKIN it for now..
       this.app.use(vhost('www.thesact.gr', appApi));
       this.app.use(vhost('ath.weareroots.org', appApi));
+      this.app.use(vhost('ath.thesact.gr', appApi));
     }
 
     // ultimate fallback if no vhost triggers, use main web app
