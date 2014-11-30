554c6401d5dc68d7609317aa332241eeaa8ceb17
Thanasis Polychronakis <thanpolas@gmail.com>
2014-11-30 11:09:05 +0200

    Add CRUD forms on community controller

--- back/controllers/city/community.ctrl.js
+++ back/controllers/city/community.ctrl.js
@@ -7,6 +7,7 @@ var ControllerBase = require('nodeon-base').ControllerBase;
 var CrudeEntity = require('crude-entity');
 
 var CommunityEnt = require('../../entities/community.ent');
+var CommunityMidd = require('../../middleware/communities.midd');
 
 /**
  * Communities CRUD controller.
@@ -14,7 +15,7 @@ var CommunityEnt = require('../../entities/community.ent');
  * @contructor
  * @extends {app.ControllerBase}
  */
-var Dashboard = module.exports = ControllerBase.extendSingleton(function(){
+var Community = module.exports = ControllerBase.extendSingleton(function () {
   this.crude = new CrudeEntity('/community', CommunityEnt);
   this.crude.config({
     idField: '_id',
@@ -24,4 +25,48 @@ var Dashboard = module.exports = ControllerBase.extendSingleton(function(){
     entityReadLimit: 'readLimitApi',
     entityUpdate: 'updateApi',
   });
+
+  var communityMidd = CommunityMidd.getInstance();
+
+  this.add = [
+    this._add.bind(this),
+  ];
+  this.del = [
+    communityMidd.populateSingleFromParam.bind(communityMidd),
+    this._del.bind(this),
+  ];
+  this.edit = [
+    communityMidd.populateSingleFromParam.bind(communityMidd),
+    this._edit.bind(this),
+  ];
 });
+
+/**
+ * The add an item form.
+ *
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ */
+Community.prototype._add = function(req, res) {
+  res.render('/city/community/add');
+};
+
+/**
+ * The del an item form.
+ *
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ */
+Community.prototype._del = function(req, res) {
+  res.render('/city/community/del');
+};
+
+/**
+ * The edit an item form.
+ *
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ */
+Community.prototype._edit = function(req, res) {
+  res.render('/city/community/edit');
+};
